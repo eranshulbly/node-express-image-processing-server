d66455465342d62cf1f550243483b138715c2ab7
Anshul Agarwal <er.anshulbly@gmail.com>
2020-09-12 19:43:32 +0530

    resolved errors

--- api/src/router.js
+++ api/src/router.js
@@ -1,46 +1,44 @@
-const express = require('express');
+const { Router } = require("express");
+const multer = require("multer");
 
-const multer = require('multer');
+const router = Router();
 
-const router = express.Router()
+const filename = (request, file, callback) => {
+  callback(null, file.originalname);
+};
 
-function filename(request, file, callback){
-    callback(null, file.originalname)
-}
+const app = require("../app");
+const { response } = require("../app");
+const { json } = require("body-parser");
 
-const app = require('../app');
-const { response } = require('../app');
-const { json } = require('body-parser');
 const storage = multer.diskStorage({
-    destination: 'api/uploads/',
-    filename: filename()
-})
+  destination: "api/uploads/",
+  filename: filename,
+});
 
-function fileFilter(request,file,callback){
-    if(file.mimetype != 'image/png'){
-        request.fileValidationError = 'Wrong file type'
-        callback(null,false, request.fileValidationError)
-    }else{
-        callback(null,true)
-    }
-}
+const fileFilter = (req, file, callback) => {
+  if (file.mimetype !== "image/png") {
+    req.fileValidationError = "Wrong file type";
+    callback(null, false, new Error("Wrong file type"));
+  } else {
+    callback(null, true);
+  }
+};
 
 const upload = multer({
-    fileFilter: fileFilter(),
-    storage: storage
-})
+  fileFilter: fileFilter,
+  storage: storage,
+});
 
-router.post('/upload', upload.single('photo', (request,response)=>{
-    if(request.fileValidationError){
-        response.status(400)
-        json({
-            error: request.fileValidationError
-        })
-    }else{
-        response.status(201)
-        json({
-            success:true
-        })
-    }
-}))
-module.exports = router
+router.post("/upload", upload.single("photo"), (request, response) => {
+  if (request.fileValidationError) {
+    response.status(400).json({
+      error: request.fileValidationError,
+    });
+  } else {
+    response.status(201).json({
+      success: true,
+    });
+  }
+});
+module.exports = router;
